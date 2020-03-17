83a7f96af22fe401aa410910c6a1e52286cdaa70
Andre Entrekin <andre.entrekin@gmail.com>
2020-03-17 16:11:39 -0500

    expaned readme

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const GMAIL_USER = process.env.GMAIL_USER
+const GMAIL_PASS = process.env.GMAIL_PASS
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// POST route from contact form
+app.post('/contact', (req, res) => {
+
+    // Instantiate the SMTP server
+    const smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        }
+    })
+
+    // Specify what the email will look like
+    const mailOpts = {
+        from: 'Your sender info here', // This is ignored by Gmail
+        to: GMAIL_USER,
+        subject: 'New message from contact form at tylerkrys.ca',
+        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    }
+
+    // Attempt to send the email
+    smtpTrans.sendMail(mailOpts, (error, response) => {
+        if (error) {
+            res.render('contact-failure') // Show a page indicating failure
+        }
+        else {
+            res.render('contact-success') // Show a page indicating success
+        }
+    })
+})
--- readme.md
+++ readme.md
@@ -1,17 +1,20 @@
-Porfolio
+# Porfolio
 Thank you for visiting my porfolio page! This is a place where I continue to gather and post some of my skills and accomplishments.
 
-Table of Contents:
--Usage
--Future Updates
--Thanks
-Usage
+[You may also find my resume:](https://docs.google.com/document/d/1LKi49g8pl9S7DhcJ_kRN7VdZnrS_648mi2wNJ2usF2w/edit?usp=sharing "Andre Entrekin's CV")
+
+## Table of Contents:
+    Usage
+    Future Updates
+    Thanks
+
+## Usage
 The site is a single page portfolio allowing for all of my works to be visible without having to click through multiple pages.
 
-Future Updates
+## Future Updates
 In the future, I will be adding on an extra page just for my photos since there are way too many to be shown on just a single page that is not solely dedicated to photos
 
-Thanks to
+## Thanks to
 Mert Cukuren https://codepen.io/knyttneve/pen/YgZbLO for some css design tools used on the photography section.
 
 Kilian Kremer, Lukasz Szmigiel, Sebastian Unrau, Veeterzy Smqil for the use of their photos as background images.
