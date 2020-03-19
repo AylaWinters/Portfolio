dd7d55d991c5255ffbe2dbd2e0612a4a3eb83831
Andre Entrekin <andre.entrekin@gmail.com>
2020-03-19 15:26:50 -0500

    added resume

--- routes/HTMLroutes.js
+++ routes/HTMLroutes.js
@@ -2,8 +2,10 @@ var path = require("path")
 
 module.exports = app => {
     app.get("/", function (req, res) {
-
         res.sendFile(path.join(__dirname, "index.html"));
+    });
+    app.get("/resume", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/resume.html"));
     });
 
 }
