d34d93de2cb2539d2b71a0d5f4756c207050970d
Andre Entrekin <andre.entrekin@gmail.com>
2021-04-28 13:40:46 -0500

    disable mail

--- public/mail.js
+++ public/mail.js
@@ -1,33 +1,33 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const mailgun = require("nodemailer-mailgun-transport");
-require("dotenv").config();
+// const mailgun = require("nodemailer-mailgun-transport");
+// require("dotenv").config();
 
-const auth = {
-  auth: {
-    api_key: process.env.MAILGUN_APIKEY,
-    domain: process.env.MAILGUN_DOMAIN,
-  },
-};
+// const auth = {
+//   auth: {
+//     api_key: process.env.MAILGUN_APIKEY,
+//     domain: process.env.MAILGUN_DOMAIN,
+//   },
+// };
 
-const transporter = nodemailer.createTransport(mailgun(auth));
+// const transporter = nodemailer.createTransport(mailgun(auth));
 
-const sendMail = (name, email, message, cb) => {
-  const mailOptions = {
-    from: email,
-    to: "andre.entrekin@gmail.com",
-    subject: "New Message from AndreEntrekin.com",
-    text: `${name} says:
-    ${message}`,
-  };
+// const sendMail = (name, email, message, cb) => {
+//   const mailOptions = {
+//     from: email,
+//     to: "andre.entrekin@gmail.com",
+//     subject: "New Message from AndreEntrekin.com",
+//     text: `${name} says:
+//     ${message}`,
+//   };
 
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      cb(err, null);
-    } else {
-      cb(null, data);
-    }
-  });
-};
+//   transporter.sendMail(mailOptions, (err, data) => {
+//     if (err) {
+//       cb(err, null);
+//     } else {
+//       cb(null, data);
+//     }
+//   });
+// };
 
-module.exports = sendMail;
+// module.exports = sendMail;
